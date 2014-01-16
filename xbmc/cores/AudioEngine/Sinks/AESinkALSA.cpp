42770c04d0a591a0b01c5b1c317862288efc97b8
croniccorey <cronmod.dev@gmail.com>
2014-01-15 23:51:32 -0500

    [AML] Remap non-working sample rates for M6 or newer devices
    M6 Sample rates:
    
    //No Audio
    - 5512 16/24
    - 8000 16/24
    - 11025 16/24
    - 16000 16/24
    - 22050 16/24
    - 24000 16/24
    
    //Working Audio
    - 32000 16/24
    - 44100 16/24
    - 48000 16/24
    - 64000 16/24
    - 88200 16/24
    - 96000 16/24
    - 176400 16
    - 192000 16/24
    - 384000 16
    
    //Popping/Crackling
    - 176400 24
    - 384000 24
    
    Conflicts:
    	xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp

--- xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
@@ -307,27 +307,54 @@ bool CAESinkALSA::InitializeHW(const ALSAConfig &inconfig, ALSAConfig &outconfig
 
   unsigned int sampleRate   = inconfig.sampleRate;
 #if defined(HAS_LIBAMCODEC)
-  // alsa/kernel lies, so map everything to 44100 or 48000
-  switch(sampleRate)
-  {
-    case 5512:
-    case 11025:
-    case 22050:
-    case 88200:
-    case 176400:
-      sampleRate = 44100;
-      break;
-    case 8000:
-    case 16000:
-    case 24000:
-    case 32000:
-    case 64000:
-    case 96000:
-    case 192000:
-    case 384000:
-      sampleRate = 48000;
-      break;
-  }
+  if (aml_get_device_type() != AML_DEVICE_TYPE_UNKNOWN
+    && aml_get_device_type() >= AML_DEVICE_TYPE_M6)
+    {
+       // M6 or Newer - Remap non-working sample rates
+       switch(sampleRate)
+       {
+         case 5512:
+         case 11025:
+         case 22050:
+           sampleRate = 44100;
+           break;
+         case 176400:
+           sampleRate = 88200;
+           break;
+         case 8000:
+         case 16000:
+         case 24000:
+           sampleRate = 32000;
+           break;
+         case 384000:
+           sampleRate = 192000;
+           break;
+       }
+    }
+  else
+    {
+       // M3 or Older - Map everything to 44100 or 48000
+       switch(sampleRate)
+       {
+         case 5512:
+         case 11025:
+         case 22050:
+         case 88200:
+         case 176400:
+           sampleRate = 44100;
+           break;
+         case 8000:
+         case 16000:
+         case 24000:
+         case 32000:
+         case 64000:
+         case 96000:
+         case 192000:
+         case 384000:
+           sampleRate = 48000;
+           break;
+       }
+    }
 #endif
   unsigned int channelCount = inconfig.channels;
   snd_pcm_hw_params_set_rate_near    (m_pcm, hw_params, &sampleRate, NULL);
