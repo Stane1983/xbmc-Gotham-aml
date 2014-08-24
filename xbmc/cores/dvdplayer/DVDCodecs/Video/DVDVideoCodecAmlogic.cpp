e1666bcdfdd8b5403dfd5592780c26d702ed2fe9
Alex Deryskyba <alex@codesnake.com>
2014-08-24 21:39:58 +0300

    [AML] Remove dependency on libamplayer and amffmpeg

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
@@ -130,9 +130,6 @@ bool CDVDVideoCodecAmlogic::Open(CDVDStreamInfo &hints, CDVDCodecOptions &option
     case AV_CODEC_ID_H263I:
       m_pFormatName = "am-h263";
       break;
-    case AV_CODEC_ID_FLV1:
-      m_pFormatName = "am-flv1";
-      break;
     case AV_CODEC_ID_RV10:
     case AV_CODEC_ID_RV20:
     case AV_CODEC_ID_RV30:
